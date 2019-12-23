366899cbfb762c9daf6efc8b1d6b490a71711844
wuling31715 <wuling31715@gmail.com>
2019-12-23 10:48:07 +0800

    .

--- 1.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def twoSum(self, nums, target: int):
-        for i, n in enumerate(nums):
-            for j, m in enumerate(nums[i+1:]):
-                if n + m == target:
-                    return [i, j+i+1]
